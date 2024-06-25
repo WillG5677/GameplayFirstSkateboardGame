e45bffc5674dc922352d64a293fe98fa8c3e1968
WillGauthier <willg5677@gmail.com>
2024-06-24 22:22:40 -0700

    Borrowed scripts from Mix and Jam ACDialogue project
    Imported and edited down barebones scripts necessary for Simmlish system

---
+++ NewSkateboardGame/Assets/_Scripts/AnimalCrossingDialogue/DialogueAudio.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DialogueAudio : MonoBehaviour
+{
+    public AudioClip[] voices;
+    public AudioClip[] punctuations;
+    [Space]
+    public AudioSource voiceSource;
+    public AudioSource punctuationSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMP_Animated.instance.onTextReveal.AddListener((newChar) => ReproduceSound(newChar));
+    }
+
+    public void ReproduceSound(char c)
+    {
+        if (char.IsPunctuation(c) && !punctuationSource.isPlaying)
+        {
+            voiceSource.Stop();
+            punctuationSource.clip = punctuations[Random.Range(0, punctuations.Length)];
+            punctuationSource.Play();
+        }
+
+        if (char.IsLetter(c) && !voiceSource.isPlaying)
+        {
+            punctuationSource.Stop();
+            voiceSource.clip = voices[Random.Range(0, voices.Length)];
+            voiceSource.Play();
+        }
+
+    }
+
+
+
+}
