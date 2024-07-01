507c64edae6229391656fecbc369f0d5167883af
qcluu <lcao29@gmail.com>
2024-06-30 23:31:22 -0400

    falling animation

--- NewSkateboardGame/Assets/_Scripts/PlayerMovement.cs
+++ NewSkateboardGame/Assets/_Scripts/PlayerMovement.cs
@@ -64,6 +64,7 @@ public class PlayerMovement : MonoBehaviour
         animator.SetBool("isGrinding", isGrinding);
         animator.SetBool("isJumping", !grounded);
         animator.SetFloat("Speed", GetSpeed());
+        animator.SetBool("canMove", canMove);
 
         if (isGrinding)
         {
